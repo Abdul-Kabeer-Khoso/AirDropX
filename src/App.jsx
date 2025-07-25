3e689cd311a83e9edbb3a55576e95b0d8b21d25c
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2025-07-25 20:08:01 +0500

    Add backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OuterContainer from "./components/OuterContainer";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <OuterContainer />
